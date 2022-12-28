c4f2f07fafe9daf62779b972228d5a0b1310dd6c
utkarshuv <utkarshverma7d@gmail.com>
2022-12-29 00:55:59 +0530

    adding basic endpoints

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')  #just a / means on the base URL
-def endpoint_1(): #this method can be named anything
-    return jsonify(message='first end point')
+@app.route('/home')
+def home_page(): #this method can be named anything
+    # return jsonify(message='first end point')
+    return "<h1>Home Page</h1>"
+
+@app.route('/about')
+def about_page():
+    return "<h1>About Page</h1>"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
